c6e4b59b136d90c6d1871ae6ff72a8e7e69fe928
Alan Jair Guadalupe Marinez <alanuadalupe1665@gmail.com>
2023-05-20 19:56:39 -0600

    add function createEmployers and fix errors

--- src/controllers/employersControllers.js
+++ src/controllers/employersControllers.js
@@ -31,7 +31,73 @@ const getEmployer = async  (req, res) => {
     })
 }
 
+const createEmployer = (req, res) => {
+    const user = req.user;
+    const auth_cookie = req.auth_token;
+    res
+    .render('createEmployer', {
+        user: user.nameacount,
+        email: user.email,
+        auth_token: auth_cookie
+    })
+}
+
+const createEmployerPost = async (req, res) => {
+    if(!req.body.complete_name){
+        res
+        .status(400)
+        .json({
+            success: false,
+            message: 'El nombre completo es requerido'
+        })
+        return;
+    }
+
+    const matircula = await getValidMatricula();
+    const complete_name = req.body.complete_name;
+    const query_create_employer = 'INSERT INTO empleados (MATRICULA, Nombre) VALUES (?, ?)';
+    const pool = await MySQL.getPool();
+    try {
+        const result_create_employer = await pool.query(query_create_employer, [matircula, complete_name]);
+        res.json({
+            success: true,
+            message: 'Empleado creado con exito',
+            name: complete_name,
+            matricula: matircula
+        })
+    } catch (error) {
+        console.log(error);
+        res
+        .status(500)
+        .json({
+            success: false,
+            message: 'Error al crear el empleado'
+        })
+        return;
+    }
+
+}
+
+
+async function getValidMatricula(){
+    const matricula = Math.floor(Math.random() * 1000000000);
+
+    const query_get_matricula = 'SELECT matricula FROM empleados WHERE matricula = ?';
+
+    const pool = await MySQL.getPool();
+
+    const result_matricula = await pool.query(query_get_matricula, [matricula]);
+
+    if(result_matricula[0].length > 0){
+        return getValidMatricula();
+    }else{
+        return matricula;
+    }
+}
+
 
 module.exports = {
-    getEmployer
+    getEmployer,
+    createEmployer,
+    createEmployerPost
 }
--- src/routes/employersRoutes.js
+++ src/routes/employersRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const { getEmployer } = require('../controllers/employersControllers');
+const { getEmployer, createEmployer, createEmployerPost } = require('../controllers/employersControllers');
 const verifyCookies = require('../middlewares/verifyCookies');
 
 router.get('/employer/:matricula', verifyCookies.verifyCookies, getEmployer )
+
+router.get('/createEmpleado', verifyCookies.verifyCookies, createEmployer )
+
+router.post('/employer/createEmployer', verifyCookies.verifyCookies, createEmployerPost)
 
 module.exports = router;
